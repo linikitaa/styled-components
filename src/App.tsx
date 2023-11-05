b25aa03ea3dc86b38195a1684226a6d264d7c4b2
LINIKITAA <linikitaa@yandex.ru>
2023-11-05 13:50:20 +0300

    Commit(1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,52 @@
 import React from 'react';
 import './App.css';
+import styled from "styled-components";
+import {StyledBtn, SuperButton} from './components/Button.styled';
+import {StyledLink} from "./components/Link.styled";
+import {Menu} from "./components/Menu.styled";
+
 
 function App() {
   return (
     <div className="App">
+        <Menu>
+            <ul>
+                <li><a href="#">Menu item 1</a></li>
+                <li><a href="#">Menu item 2</a></li>
+                <li><a href="#">Menu item 3</a></li>
+            </ul>
+        </Menu>
+        <Box>
+            <StyledBtn as={StyledLink} href={'#'}>LinkComponent</StyledBtn>
+            <StyledBtn as='a' href={'#'}>Link</StyledBtn>
+            <StyledBtn>Hello</StyledBtn>
+            <SuperButton>SuperButton</SuperButton>
+        </Box>
 
     </div>
   );
 }
 
 export default App;
+
+
+
+const Box = styled.div `
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  
+  button {
+    cursor: pointer;
+  }
+
+  ${StyledLink} {
+  cursor: zoom-in;
+  }
+  
+  @media screen and (max-width: 800px) {
+    flex-direction: column;
+  }
+`
---
+++ src/components/Link.styled.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledLink = styled.a `
+  background-color: #10c5c5;
+  font-size: 2rem;
+  font-weight: bold;
+`
